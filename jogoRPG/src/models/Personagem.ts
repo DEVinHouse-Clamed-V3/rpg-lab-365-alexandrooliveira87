03c94dbe3a466af93d6c3e89fb1e55770e760cb3
Alexandro Oliveira <=>
2024-11-22 15:21:12 -0300

    Correção gramatical e indentação do código.

--- jogoRPG/src/models/Personagem.ts
+++ jogoRPG/src/models/Personagem.ts
@@ -46,22 +46,25 @@ export default class Personagem {
 
     public setArma(arma: Arma | null): void {
         this.arma = arma;
+
     }
 
     // Métodos
     public atacar(alvo: Personagem): void {
         const chanceDeAtaque = Math.random(); // Chance de falhar no ataque (50%)
         if (chanceDeAtaque < 0.5) {
+
             console.log(`${this.nome} tentou atacar, mas falhou!`);
             return;
         }
 
-        const dano = this.calcularDano();
+        const dano = this.calcularDano() ;
         console.log(`${this.nome} atacou ${alvo.getNome()} causando ${dano} de dano!`);
         alvo.receberDano(dano);
     }
 
     public receberDano(dano: number): void {
+
         this.vida -= dano;
         if (this.vida > 0) {
             console.log(`${this.nome} recebeu ${dano} de dano. Vida restante: ${this.vida}`);
@@ -71,13 +74,16 @@ export default class Personagem {
     }
 
     public equiparArma(arma: Arma): void {
+
         this.arma = arma;
         console.log(`${this.nome} equipou a arma: ${arma.getNome()}`);
     }
 
     private calcularDano(): number {
+        
         const danoBase = this.forca;
         const danoArma = this.arma ? this.arma.getDano() : 0;
         return danoBase + danoArma;
     }
+
 }
